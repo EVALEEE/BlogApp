019ce8ee8a7542fab04414e9600bb9851fe64052
Eva Lee <u7829183@anu.edu.au>
2025-02-01 19:16:19 +0800

    exersices 4.3.-4.7 is on going

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,23 +1,25 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
 //测试文件导入要测试的函数，并将其分配给名为 reverse 的变量
 const reverse = require('../utils/for_testing').reverse
 
-test('reverse of a', () => {
-    const result = reverse('a')
+describe('reverse', () => {
+    test('reverse of a', () => {
+        const result = reverse('a')
 
-    assert.strictEqual(result, 'a')
-})
+        assert.strictEqual(result, 'a')
+    })
 
-test('reverse of react', () => {
-    const result = reverse('react')
+    test('reverse of react', () => {
+        const result = reverse('react')
 
-    assert.strictEqual(result, 'tcaer')
-})
+        assert.strictEqual(result, 'tcaer')
+    })
 
-test('reverse of saippuakauppias', () => {
-    const result = reverse('saippuakauppias')
+    test('reverse of saippuakauppias', () => {
+        const result = reverse('saippuakauppias')
 
-    assert.strictEqual(result, 'saippuakauppias')
+        assert.strictEqual(result, 'saippuakauppias')
+    })
 })
