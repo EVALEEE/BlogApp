8a67d8d4eafcd7d8e0bf3f8f40c7a290df593e08
Eva Lee <u7829183@anu.edu.au>
2025-02-09 17:29:05 +0800

    finished 4.18-4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -28,13 +28,24 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.status === 400) {
     return response.status(400).json({ error: error.message });
   }
-
-
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  // code that extracts the token
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+    console.log("request.token:", request.token)
+  } else {
+    request.token = null
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
